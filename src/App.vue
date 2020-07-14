4bc826de734ffbbfe651eb56dd492d33365f862f
huanghoufeng176 <287157014@qq.com>
2020-07-14 13:20:00 +0800

    diyici

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    abc
+  </div>
+</template>
+
+<script>
+export default {
+  components:{},
+  data(){
+    return {
+    }
+  },
+  created(){},
+  mounted(){},
+  watch:{},
+  computed:{},
+  methods:{},
+}
+</script>
+<style scoped>
+</style>
